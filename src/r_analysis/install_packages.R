e0a20c325a139f9d05ecf5970f85d2d8713688f2
QuentinKunYu <89042708+QuentinKunYu@users.noreply.github.com>
2025-10-27 23:55:35 -0500

    Implement R analysis script

---
+++ src/r_analysis/install_packages.R
@@ -0,0 +1,7 @@
+# install_packages.R
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# Install necessary packages
+install.packages("readr")
+install.packages("dplyr")
+install.packages("caret") 
